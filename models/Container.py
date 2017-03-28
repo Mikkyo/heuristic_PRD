21cc023b27517e0bfe51163b29208ef626b6ca5f
Unknown <quentinbouvier@laposte.net>
2017-03-28 16:41:33 +0200

    Updating Container

--- models/Container.py
+++ models/Container.py
@@ -10,7 +10,7 @@ class Container:
     # Private
     _capacity = None  # Resource Object
     _node = None  # Node Object
-    _app = None # App Object
+    _application = None # App Object
 
     _base_priority = None # natural priority
     _priority = None # real priority
@@ -19,29 +19,51 @@ class Container:
     _last_used = None
 
     # --- Constructor
-    def __init__(self, capacity, node, app, base_priority):
-        self._capacity = capacity
+    def __init__(self, resource, node, app, base_priority):
+        self._capacity = resource
         self._node = node
-        self._app = app
+        self._application = app
         self._base_priority = base_priority
         self._priority = base_priority
         self._tasks = []
         self._last_used = 0
 
+    # --- Methods
+    # Method to add task to a container
+    def add_task(self, task, simulation_date):
+        self._tasks.append(task)
+        task.start()
+        self.update(simulation_date)
+
+    # Method to remove a task
+    def remove_task(self, task, simulation_date):
+        count = self._tasks.count(task)
+        if count > 0 :
+            self._tasks.remove(simulation_date)
+            self.update(simulation_date)
+
+    # Method to preempt a container
+    def preempt(self, simulation_date):
+        for i in range(0, len(self._tasks)):
+            self._tasks[i].preempt()
+            self.remove_task(self._tasks[i], simulation_date)
+        self._application.remove_container(self)
+        self._node.remove_container(self)
+
+    # Method to update the container
+    def update(self, simulation_date):
+        # P37 of the Nicolas Gougeon PRD, written in a differnt way
+        A = 0.0
+        for i in range(0, len(self._tasks)):
+            A = max(A, pow(min(self._tasks[i].progress, 1.0), 1/4))
+        self._priority = A * self._base_priority
+
+        if len(self._tasks) > 0:
+            self._last_used = simulation_date
+
 
     # --- Getters/Setters
-    # Vcores
-    @property
-    def vcores(self):
-        return self._v_cores
-
-    @vcores.setter
-    def vcores(self, value):
-        self._vcores = value
-
-    @vcores.deleter
-    def vcores(self):
-        self._vcores = None
+    #
 
 
 
